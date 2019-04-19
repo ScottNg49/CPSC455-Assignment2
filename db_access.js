5fde3f9d08459609cae220ab41335352c496a91a
ScottNg49 <30333501+ScottNg49@users.noreply.github.com>
2019-04-19 13:50:41 -0700

    updated main-s.js, incomplete

--- db_access.js
+++
@@ -1,8 +0,0 @@
-// module for accessing db
-// will be using txt file
-
-const fs = require('fs')
-var file = 'mydb.txt'
-var authorized_users = fs.readFileSync(file,'utf-8')
-
-console.log(authorized_users)
