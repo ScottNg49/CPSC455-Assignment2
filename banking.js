ed428b8d7ede05aa60889e483e6d30ad439a2a3a
block3d <block3d@csu.fullerton.edu>
2019-04-18 22:15:22 -0700

    Update Banking,js

--- banking.js
+++ banking.js
@@ -7,13 +7,29 @@ const bodyParser= require("body-parser");
 
 //Create th express app.
 var app=express();
+app.use(sessions({
+cookieName: 'session',
+secret: 'random_string_goes_here',
+duration: 30*60*1000,
+activeDuration:5*60*1000,
+
+}));
 //Needed to use post/parse the request body
 app.use(bodyParser.urlencoded({ extended: true}));
+app.get('/',function(req,res){
 
-app.post('/login',function(req,res)){
+res.sendFile(__dirname + "/index.html");
+res.redirect('/login');
+
+console.log(username);
+
+});
+app.post('/login',function(req,res){
+  var username=req.body.username;
+  var password=req.body.password;
+  console.log(username);
 
 
 
-
-}
+});
 app.listen(3000);
