7c1e4e5bb9d99e11940de7bda3f4154b5beaf5d4
ScottNg49 <30333501+ScottNg49@users.noreply.github.com>
2019-04-21 18:37:01 -0700

    updated,
     still need to add 'functionality' to connect to databas

--- dashboard.js
+++ dashboard.js
@@ -13,7 +13,7 @@ app.get('/', function(req,res) {
 
 });
 
-app.get('/dashboard', function(req,res) {
+app.use('/dashboard', function(req,res) {
     // res.write(Users[0])
     var name = "Scott"
     var page = "<html>"
@@ -49,39 +49,39 @@ app.get('/dashboard', function(req,res) {
 
 });
 
-app.get('/deposit', function(req,res) {
+app.get('/deposit', function(req,res, next) {
     // res.send('deposit')
     var page = "<!DOCTYPE html>"
     page += "<html>"
 
-    // xml function
+    // xml data passing
     page += "<script>"
     page += "function loadDoc() {"
+    page += "if(document.getElementById(\"deposit\").value > 10000) {"
+    page += "alert('Maximum deposit $10000! Retry!'); "
+    page += " } else if (document.getElementById(\"deposit\").value < 10) {"
+    page += "alert('Minimum deposit $10! Retry!');"
+    page += " } else {"
     page += "var message = \"<?xml version='1.0'?>\"+\"<username>\"+\"<account>\"+document.getElementById('account').value+"
     page += "\"</account>\"+\"<deposit>\"+document.getElementById(\"deposit\").value+\"</deposit>\"+\"</username>\";"
     page += "var xhttp = new XMLHttpRequest();"
+
+    page += "xhttp.onreadystatechange= function() {"
+    page += "if(xhttp.readyState == 4 && xhttp.status == 200) {"
+    page += "alert('Attempting to Deposit $' + document.getElementById('deposit').value)}}; "
+
     page += "xhttp.open(\"POST\", \"/deposit_success\", false);"
-    page += "xhttp.setRequestHeader('Content-type', 'application/xml');"
+    page += "xhttp.setRequestHeader('Content-type', 'text/xml');"
     page += "xhttp.send(message);"
-    page += "alert('Deposited $' + document.getElementById(\"deposit\").value)"
-    page += "};"
-    //page += "</script>"
-
-    // will be doing server-side xss sanitization
-    //page += "<script>"
-    page += "function validate() { alert('validating')"
-    //page += "if(document.getElementById(\"deposit\").value >= 10000) {"
-    //page += "alert('invalid!')"
-    //page += "} else { loadDoc(); console.log('xml')"
-    page += "};"
-
+    //page += "alert('Attempting to Deposit $' + document.getElementById(\"deposit\").value)"
+    page += "}};"
     page += "</script>"
 
     page += "<body>"
     page += "<h1>Northside Banking Deposit Page</h1><br><br>"
 
     // start form
-    page += "<form>"
+    page += "<form onsubmit='return loadDoc();'>"
     // page += "<form action='/deposit_success' method='POST'>"
 
     // drop down menu
@@ -95,7 +95,7 @@ app.get('/deposit', function(req,res) {
     // deposit user input
     page += "<label for='deposit'>Deposit between $10 and $10000   </label>" 
     page += "<input type='number' id='deposit' name='deposit' value='0' min='10' max='10000' required>"
-    page += "<input type='submit' value='Confirm' onsubmit=\"validate()\">"
+    page += "<input type='submit' value='Confirm'>"
     page += "</form>"
     
     // go to main page
@@ -105,14 +105,16 @@ app.get('/deposit', function(req,res) {
     page += "</body></html>"
 
     res.send(page)
+
+    next();
 });
 
 app.post('/deposit_success', function(req,res) {
-    console.log(req.body)
-    console.log("aasdfsafsafasf")
-    
     // do back end processing here
+    console.log(req.body)
+    console.log("Deposit Success")
 
+    res.end()
 });
 
 app.get('/withdraw', function(req,res) {
@@ -120,23 +122,29 @@ app.get('/withdraw', function(req,res) {
 
     var page = "<html>"
 
-    // xml function
+    // xml data passing
     page += "<script>"
     page += "function loadDoc() {"
+    page += "if(document.getElementById(\"withdraw\").value > 10000) {"
+    page += "alert('Maximum withdraw $10000! Retry!'); "
+    page += " } else if (document.getElementById(\"withdraw\").value < 10) {"
+    page += "alert('Minimum withdraw $10! Retry!');"
+    page += " } else {"
     page += "var message = \"<?xml version='1.0'?>\"+\"<username>\"+\"<account>\"+document.getElementById('account').value+"
     page += "\"</account>\"+\"<withdraw>\"+document.getElementById(\"withdraw\").value+\"</withdraw>\"+\"</username>\";"
     page += "var xhttp = new XMLHttpRequest();"
     page += "xhttp.open(\"POST\", \"/withdraw_success\", false);"
     page += "xhttp.setRequestHeader('Content-type', 'application/xml');"
     page += "xhttp.send(message);"
-    page += "alert('Withdrew $' + document.getElementById(\"withdraw\").value)"
-    page += "}"
+    page += "alert('Attempting to wirthdraw $' + document.getElementById(\"withdraw\").value)"
+    page += "}};"
+    page += "</script>"
 
     page += "<body>"
     page += "<h1>Northside Banking Withdrawing Page</h1><br><br>"
 
     // start form
-    page += "<form action='/withdraw_success' method='POST'>"
+    page += "<form onsubmit='return loadDoc()'>"
 
     // drop down menu
     page += "<label for='account'>Choose an account   </label>"
@@ -147,8 +155,8 @@ app.get('/withdraw', function(req,res) {
     page += '</select><br><br>'
 
     // withdraw user input
-    page += "<label for='withdraw'>Amount to Withdraw   </label>" 
-    page += "<input type='number' id='withdraw' name='withdraw' value='0' min='1' max='10000' required>"
+    page += "<label for='withdraw'>Withdraw between $10 and $10000 at a time  </label>" 
+    page += "<input type='number' id='withdraw' name='withdraw' value='0' min='10' max='10000' required>"
     page += "<input type='submit' value='Confirm'>"
     page += "</form>"
 
@@ -163,18 +171,43 @@ app.get('/withdraw', function(req,res) {
 
 app.post('/withdraw_success', function(req,res) {
     console.log(req.body)
-    console.log("aasdfsafsafasf")
-    
+    console.log("withdraw_success")
+
+    res.end()
+
     // do back end processing here
 })
 
 app.get('/transfer', function(req,res) {
     var page = "<html>"
-    page += "<form action='/transfer_success' method='POST'>"
+
+    // xml data passing
+    page += "<script>"
+    page += "function loadDoc() {"
+
+    page += "var message = \"<?xml version='1.0'?>\"+\"<username>\"+"
+    page += "\"<sender>\"+document.getElementById('sender').value+\"</sender>\"+"
+    page += "\"<receiver>\"+document.getElementById(\"receiver\").value+\"</receiver>\"+"
+    page += "\"<transfer>\"+document.getElementById('transfer').value+ \"</transfer>\"+"
+    page += "\"</username>\";"
+    page += "var xhttp = new XMLHttpRequest();"
+    page += "xhttp.open(\"POST\", \"/transfer_success\", false);"
+    page += "xhttp.setRequestHeader('Content-type', 'application/xml');"
+    page += "xhttp.send(message);"
+    page += "alert('Attempting to transfer $' + document.getElementById(\"transfer\").value + ' from ' + document.getElementById(\"sender\").value +"
+    page += "' to ' + document.getElementById(\"receiver\").value)"
+    page += "};"
+    page += "</script>"
+
+    page += "<body>"
+    page += "<h1>Northside Banking Transfer Page</h1><br><br>"
+
+    // form start
+    page += "<form onsubmit='loadDoc()'>"
     page += "<label for='account'>Choose an account to transfer from   </label>"
 
     // drop down menu account 1
-    page += "<select>"
+    page += "<select id=sender>"
     page += "<option value='Dummy_1'>Dummy_1</option>"
     page += "<option value='Dummy_2'>Dummy_2</option>"
     page += "<option value='Dummy_3'>Dummy_3</option>"
@@ -182,7 +215,7 @@ app.get('/transfer', function(req,res) {
 
     // drop down menu account 2
     page += "<label for='account'>Choose an account to transfer to   </label>"
-    page += "<select>"
+    page += "<select id=receiver>"
     page += "<option value='Dummy_1'>Dummy_1</option>"
     page += "<option value='Dummy_2'>Dummy_2</option>"
     page += "<option value='Dummy_3'>Dummy_3</option>"
@@ -192,13 +225,24 @@ app.get('/transfer', function(req,res) {
     page += "<label for='transfer'>Transfer between $1 and $10000   </label>" 
     page += "<input type='number' id='transfer' value=0 name='transfer' min='1' max='10000' required>"
     page += "<input type='submit' value='Confirm'>"
+    page += "</form>"
 
-    page += "</form></html>"
+    // go to main page
+    page += "<a href='http://localhost:3000/dashboard'>"
+    page += "<button>Main Page</button> </a><br><br>"
+
+    // closing tags
+    page += "</body></html>"
 
     res.send(page)
 });
 
-app.post('/transfer_success')
+app.post('/transfer_success', function(req,res) {
+    console.log(req.body)
+    console.log("transfer_success")
+
+    res.end()
+})
 
 app.listen(3000);
 
